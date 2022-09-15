361828c4cd196fc9a31e0d1ab9595060b32c01ea
hso-sue <hso112277@yahoo.co.jp>
2022-09-15 23:29:40 +0900

    口コミ投稿フォームビューの修正

--- app/controllers/evaluation_comments_controller.rb
+++ app/controllers/evaluation_comments_controller.rb
@@ -1,11 +1,11 @@
 class EvaluationCommentsController < ApplicationController
   def create
     comment = EvaluationComment.create(comment_params)
-    redirect_to root_path
+    redirect_to "/shops/#{comment.shop.id}"
   end
 
   private
   def comment_params
-    params.require(:evaluation_comment).permit(:title, :content).merge(user_id: current_user.id, shop_id: params[:shop_id])
+    params.require(:evaluation_comment).permit(:title, :content,:rate).merge(user_id: current_user.id, shop_id: params[:shop_id])
   end
 end
